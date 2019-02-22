787e477c32e1d5e70f6e9fe8ed66342abf6b878b
SimoneBronzini <simone.bronzini@gmail.com>
2019-02-22 12:17:45 +0100

    Bump package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='moveax-sdk-boilerplate',
-      version='1.0.0',
+      version='1.0.1',
       packages=find_packages(),
       install_requires=['requests==2.20.0', 'moveax-validation'],
       description='A simple SDK boilerplate',
