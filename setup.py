bc15e5e83c9508a6742ab88a3a59fa4c529c2a5a
Francesco <francesco.andreozzi90@gmail.com>
2020-05-02 19:28:22 +0200

    pin moveax validation library to 1.1.2 version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='moveax-sdk-boilerplate',
-      version='1.1.0',
+      version='1.1.1',
       packages=find_packages(),
-      install_requires=['requests==2.20.0', 'moveax-validation'],
+      install_requires=['requests==2.20.0', 'moveax-validation==1.1.2'],
       description='A simple SDK boilerplate',
       author='MovEax',
       author_email='simone.bronzini@moveax.it',
