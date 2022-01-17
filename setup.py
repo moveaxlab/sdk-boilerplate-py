aaee3669dfa392fb8e09cb5e1b03e8dbcbf88a3d
Francesco Andreozzi <francesco.andreozzi90@gmail.com>
2022-01-17 12:31:40 +0100

    change dependency version check requests>=2.20.0,<3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(name='moveax-sdk-boilerplate',
       version='1.1.1',
       packages=find_packages(),
-      install_requires=['requests==2.20.0', 'moveax-validation==1.1.2'],
+      install_requires=['requests>=2.20.0,<3', 'moveax-validation==1.1.2'],
       description='A simple SDK boilerplate',
       author='MovEax',
       author_email='simone.bronzini@moveax.it',
